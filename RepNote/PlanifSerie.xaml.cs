aca75b7ef5d6d15506eeb919fbcb287d287d65bf
Thomas-Peltier <pv03xgo@eduvaud.ch>
2026-05-01 15:37:46 +0200

    feat(Ajout page PlanifExercice)

--- RepNote/PlanifSerie.xaml.cs
+++ RepNote/PlanifSerie.xaml.cs
@@ -6,17 +6,14 @@ namespace RepNote;
 
 public partial class PlanifSerie : ContentPage
 {
-    // La liste qui contient tes noms de séries
     public ObservableCollection<string> AddedSeries { get; set; }
 
     public PlanifSerie()
     {
         InitializeComponent();
 
-        // Initialisation de la liste
         AddedSeries = new ObservableCollection<string>();
 
-        // Liaison de la liste à la CollectionView
         SeriesList.ItemsSource = AddedSeries;
     }
 
@@ -26,10 +23,8 @@ public partial class PlanifSerie : ContentPage
 
         if (!string.IsNullOrWhiteSpace(seriesName))
         {
-            // Ajoute le nom à la liste (l'UI se met à jour toute seule)
             AddedSeries.Add(seriesName);
 
-            // Vide le champ de saisie
             SeriesEntry.Text = string.Empty;
             SeriesEntry.Unfocus();
         }
