1f7141cf3327f1d10beef49b2945a19374299cb1
Thomas-Peltier <pv03xgo@eduvaud.ch>
2026-05-01 14:42:25 +0200

    feat(ajout serie): bouton ajouter une serie

--- RepNote/PlanifSerie.xaml.cs
+++ RepNote/PlanifSerie.xaml.cs
@@ -1,9 +1,37 @@
+using System.Collections.ObjectModel;
+/*  Lieu: ETML
+    Auteur: Thomas Peltier
+    Date: 29.04.2026*/
 namespace RepNote;
 
 public partial class PlanifSerie : ContentPage
 {
-	public PlanifSerie()
-	{
-		InitializeComponent();
-	}
+    // La liste qui contient tes noms de séries
+    public ObservableCollection<string> AddedSeries { get; set; }
+
+    public PlanifSerie()
+    {
+        InitializeComponent();
+
+        // Initialisation de la liste
+        AddedSeries = new ObservableCollection<string>();
+
+        // Liaison de la liste à la CollectionView
+        SeriesList.ItemsSource = AddedSeries;
+    }
+
+    private void OnAddSeriesClicked(object sender, EventArgs e)
+    {
+        string seriesName = SeriesEntry.Text;
+
+        if (!string.IsNullOrWhiteSpace(seriesName))
+        {
+            // Ajoute le nom à la liste (l'UI se met à jour toute seule)
+            AddedSeries.Add(seriesName);
+
+            // Vide le champ de saisie
+            SeriesEntry.Text = string.Empty;
+            SeriesEntry.Unfocus();
+        }
+    }
 }
