5f47b6f3c02ba2ea0ad9e1b4748d772a1917a1de
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-05-01 15:48:21 +0200

    feat(model):création du model workout en fonction du fichiers JSON

---
+++ RepNote/Models/WorkOut.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace RepNote.Models
+{
+    public class WorkoutRoot
+    {
+        [JsonPropertyName("workouts")]
+        public List<Workout> Workouts { get; set; } = new();
+    }
+
+    public class Workout
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("duration_seconds")]
+        public int DurationSeconds { get; set; }
+
+        [JsonPropertyName("exercises")]
+        public List<Exercise> Exercises { get; set; } = new();
+    }
+
+    public class Exercise
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("planned_sets")]
+        public List<WorkoutSet> PlannedSets { get; set; } = new();
+
+        [JsonPropertyName("actual_sets")]
+        public List<WorkoutSet> ActualSets { get; set; } = new();
+    }
+
+    public class WorkoutSet
+    {
+        [JsonPropertyName("reps")]
+        public int Reps { get; set; }
+
+        [JsonPropertyName("weight")]
+        public double Weight { get; set; }
+    }
+}
