247f6d44477ded43c78bf30e2ed87e83614e114f
Thomas-Peltier <pv03xgo@eduvaud.ch>
2026-05-20 14:32:42 +0200

    feat(shake): ajout du shake pour revenir à la page d'acceuil

--- RepNote/App.xaml.cs
+++ RepNote/App.xaml.cs
@@ -1,12 +1,61 @@
-﻿namespace RepNote
+﻿using Microsoft.Maui.Devices.Sensors;
+/*Lieu: ETML
+Auteur: Thomas Peltier
+Date: 20.05.2026*/
+namespace RepNote
 {
     public partial class App : Application
     {
+        private const double ShakeThreshold = 2.5;
+        private bool _isShaking = false;
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new AppShell();
+
+            StartListeningShake();
+        }
+
+        private void StartListeningShake()
+        {
+            if (Accelerometer.Default.IsSupported)
+            {
+                if (!Accelerometer.Default.IsMonitoring)
+                {
+                    Accelerometer.Default.ReadingChanged += OnAccelerometerReadingChanged;
+                    Accelerometer.Default.Start(SensorSpeed.Game);
+                }
+            }
+        }
+
+        private async void OnAccelerometerReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+
+            double gForce = Math.Sqrt(data.Acceleration.X * data.Acceleration.X +
+                                      data.Acceleration.Y * data.Acceleration.Y +
+                                      data.Acceleration.Z * data.Acceleration.Z);
+
+            if (gForce > ShakeThreshold && !_isShaking)
+            {
+                _isShaking = true;
+
+                try
+                {
+                    Vibration.Default.Vibrate(TimeSpan.FromMilliseconds(200));
+                }
+                catch { }
+
+                await MainThread.InvokeOnMainThreadAsync(async () =>
+                {
+                    await Shell.Current.GoToAsync("///MainPage");
+                });
+
+                await Task.Delay(1000);
+                _isShaking = false;
+            }
         }
     }
 }
